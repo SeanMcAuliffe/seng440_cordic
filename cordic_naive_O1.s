eb16824d5d6f3e577a6a7f7ef877fd3df9ce2d93
Spencer Davis <strdavis@seng440.ece.uvic.ca>
2022-08-03 16:04:58 -0700

    Annotate assembly of O1 compiler-optimized naive cordic.

--- cordic_naive_O1.s
+++ cordic_naive_O1.s
@@ -18,29 +18,29 @@ cordic_naive:
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
 	@ link register save eliminated.
-	stmfd	sp!, {r4, r5, r6, r7}
-	mov	r7, r2
-	ldr	r0, [r0, #0]
-	ldr	r1, [r1, #0]
-	mov	ip, #0
-	mov	r4, ip
-	mov	r2, ip
-	ldr	r6, .L7
-.L5:
-	cmp	r1, #0
-	addgt	r5, r0, r1, asr ip
-	subgt	r1, r1, r0, asr ip
-	ldrgt	r3, [r6, r2]
-	addgt	r4, r4, r3
-	suble	r5, r0, r1, asr ip
+	stmfd	sp!, {r4, r5, r6, r7}			; Push r4 - r7 onto stack.
+	mov	r7, r2								; Let r2 = r7.
+	ldr	r0, [r0, #0]						; r0 = x_temp_1.
+	ldr	r1, [r1, #0]						; r1 = y_temp_1.
+	mov	ip, #0								; ip = i = 0.
+	mov	r4, ip								; r4 = z_temp = 0. 
+	mov	r2, ip								; r2 = 0. r2 is the offset corresponding to i.
+	ldr	r6, .L7								; Initialize optimized access to z_table.
+.L5:										; Begin loop.
+	cmp	r1, #0								; Compare y_temp_1, 0.
+	addgt	r5, r0, r1, asr ip				; if (y_temp_1 > 0) r5 = x_temp_2 = x_temp_1 + (y_temp_1 >> i).
+	subgt	r1, r1, r0, asr ip				; if (y_temp_1 > 0) r1 = y_temp_2 = y_temp_1 - (x_temp_1 >> i).
+	ldrgt	r3, [r6, r2]					; if (y_temp_1 > 0) r3 = z_table[i].
+	addgt	r4, r4, r3						; if (y_temp_1 > 0) z_temp += z_table[i].
+	suble	r5, r0, r1, asr ip				; else . . . 
 	addle	r1, r1, r0, asr ip
 	ldrle	r3, [r6, r2]
 	rsble	r4, r3, r4
-	add	ip, ip, #1
-	add	r2, r2, #4
-	cmp	ip, #23
-	movne	r0, r5
-	bne	.L5
+	add	ip, ip, #1							; Increment i.
+	add	r2, r2, #4							; Advance by one word the offset corresponding to i.
+	cmp	ip, #23								; Evaluate loop-end condition.
+	movne	r0, r5							; if (i != 23) x_temp_1 = x_temp_2. 
+	bne	.L5									; if (i != 23) goto .L5.
 .L4:
 	str	r4, [r7, #0]
 	ldmfd	sp!, {r4, r5, r6, r7}
