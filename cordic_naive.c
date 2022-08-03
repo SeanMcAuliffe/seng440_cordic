63573e20a737d723f6aede9184ab7f8bf712ab2a
SeanMcAuliffe <sean.tristan.mcauliffe@gmail.com>
2022-08-03 13:49:46 -0700

    Rewrite timing measurement code

---
+++ cordic_naive.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+
+
+static int32_t z_table[23] = {6588397, 3889358, 2055029, 1043165, 523606, 262058,
+                                131061, 65534, 32767, 16383, 8191, 4095, 2047, 1023,
+                                 511, 255, 127, 63, 31, 15, 7, 3, 1};
+
+void cordic_naive(int32_t *x, int32_t *y, int32_t *z) {
+    int32_t x_temp_1, y_temp_1, z_temp;
+    int32_t x_temp_2, y_temp_2;
+    int32_t i;
+
+    x_temp_1 = *x;
+    y_temp_1 = *y;
+    z_temp = 0;
+
+    for (i=0; i<23; i++) {
+        if (y_temp_1 > 0) {
+            // Rotate downwards by arctan(2^-i)
+            x_temp_2 = x_temp_1 + (y_temp_1 >> i);
+            y_temp_2 = y_temp_1 - (x_temp_1 >> i);
+            z_temp += z_table[i];
+        } else {
+            // Rotate upwards by arctan(2^-i)
+            x_temp_2 = x_temp_1 - (y_temp_1 >> i);
+            y_temp_2 = y_temp_1 + (x_temp_1 >> i);
+            z_temp -= z_table[i];
+        }
+
+        x_temp_1 = x_temp_2;
+        y_temp_1 = y_temp_2;
+    }
+
+    // *x = x_temp_1;
+    // *y = y_temp_1;
+    *z = z_temp;
+}
---
+++ performance_test.c
@@ -0,0 +1,73 @@
+#include <time.h>
+#include "cordic_naive.c"
+#include "string.h"
+#include "atan_reference.c"
+
+#define NUM_ITERATIONS 10000
+#define NUM_TRIALS 1000
+
+int main(int argc, char* argv[]) {
+
+    double x_d = 0.707106;
+    double y_d = 0.707106;
+    double z_d;
+
+    int x_i = 5931642;
+    int y_i = 5931642;
+    int z_i;
+
+    clock_t timebench_begin;
+    clock_t timebench_end;
+
+    clock_t cordic_begin;
+    clock_t cordic_end;
+
+    for (int i = 1; i < argc; i++) {
+        printf("Iteration: i = %d\n", i);
+
+        if (strcmp(argv[i], "-r") == 0) {
+            printf("Running Atan Reference\n");
+            timebench_begin = clock();
+            for (int k = 0; k < NUM_TRIALS; k++) {
+                for (int j = 0; j < NUM_ITERATIONS; j++) {
+                    atan_reference(x_d, y_d, &z_d); //z_d = atan(y_d / x_d);//
+                }       
+            }
+            timebench_end = clock();
+            int time_testbench = (timebench_end - timebench_begin) / 1000;
+            printf("Atan took %d, z_d = %f\n\n", time_testbench, z_d);
+        }
+
+        if (strcmp(argv[i], "-n") == 0) {
+            printf("Running Cordic Naive\n");
+            cordic_begin = clock();
+            for (int k =0; k < 1000; k++) {
+                for (int j = 0; j < 10000; j++) {
+                    cordic_naive(&x_i, &y_i, &z_i);
+                }
+            }
+            cordic_end = clock();
+            int time_cordic = (cordic_end - cordic_begin) / 1000;
+            printf("Cordic naive took %d, z_i = %i\n\n", time_cordic, z_i);
+        }
+
+        if (strcmp(argv[i], "-o1") == 0) {
+            printf("Running Optimized 1\n");
+        }
+
+        if (strcmp(argv[i], "-o2") == 0) {
+            printf("6\n");
+            printf("Running Optimized 2\n");
+        }
+
+        if (strcmp(argv[i], "-o3") == 0) {
+            printf("Running Optimized 3\n");
+        }
+
+        if (strcmp(argv[i], "-f") == 0) {
+            printf("Running Firmware\n");
+        }
+    }
+
+    return 0;
+}
